9b2f19585462c5e11fbbda60fdb4809967ba9c50
Muaz Iqbal <muaziqbal12fast@gmail.com>
2021-08-21 21:40:52 +0500

    Updates

---
+++ precache-manifest.4919271c07914ab409b0dc98fb7cde1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69207c83a34dff8cb45b",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/css/main.afded685.chunk.css"
+  },
+  {
+    "revision": "69207c83a34dff8cb45b",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/js/main.69207c83.chunk.js"
+  },
+  {
+    "revision": "0d79864787b0a5ab1144",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/js/1.0d798647.chunk.js"
+  },
+  {
+    "revision": "cb36c962beb01b3e30e5",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/js/runtime~main.cb36c962.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "http.//muazSheikh.github.io/ReadApp/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "46d0cc078275e63de1c3adb0184f0a8a",
+    "url": "http.//muazSheikh.github.io/ReadApp/index.html"
+  }
+];
